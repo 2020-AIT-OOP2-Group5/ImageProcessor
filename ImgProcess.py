cf5d600ec5d4aa8be4ef399808d58e428c8c357f
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-03 17:27:17 +0900

    watchdogを追加

--- ImgProcess.py
+++ ImgProcess.py
@@ -1,5 +1,13 @@
 import cv2
 import numpy as np
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+target_dir = "upload_img"
+
+target_file = ["*.png","*.jpeg","*.jpg"]
 
 #1.顔を検出し、モザイク処理。
 
@@ -15,3 +23,46 @@ img_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
 
 #画像の確認
 cv2.imwrite("after_sourse.jpg",img_otsu)
+
+
+#watchdogを実装
+
+class　CheckHandler(FileSystemEventHandler):
+    #ファイル作成時のイベント
+    def on_created(self,event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        #ここはそれぞれの処理の関数
+    
+    #ファイル変更時のイベント
+    def on_modified(self,event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        #ここはそれぞれの処理の関数
+    
+    #ファイル移動時のイベント
+    def on_moved(self,event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        #ここはそれぞれの処理の関数
+
+    #ファイル削除時のイベント
+    def on_deleted(self,event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        #それぞれの処理後にあるファイルも削除
+
+
+if__name__ == "__main__":
+    #ファイルの監視を開始
+    event_handler = CheckHandler([target_file])
+    observer = Observer()
+    observer.schedule(event_handler, target_dir, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
