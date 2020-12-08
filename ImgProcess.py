d765a012d444c9492781cc6dbb2c59da441e9dd6
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-08 13:21:39 +0900

    watchdog 完了

--- ImgProcess.py
+++ ImgProcess.py
@@ -5,9 +5,7 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-target_dir = "upload_img"
-
-target_file = ["*.png","*.jpeg","*.jpg"]
+target_dir = "./upload_img"
 
 #1.顔を検出し、モザイク処理。
 
@@ -16,24 +14,16 @@ target_file = ["*.png","*.jpeg","*.jpg"]
 #3.Cannyフィルタによる輪郭抽出。
 
 #4.画像のグレースケール化（出来れば2値化も）
-img = cv2.imread("sourse.jpg",0)
-
-#閾値の自動設定
-img_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-
-#画像の確認
-cv2.imwrite("after_sourse.jpg",img_otsu)
-
 
 #watchdogを実装
 
-class　CheckHandler(FileSystemEventHandler):
+class CheckHandler(FileSystemEventHandler):
     #ファイル作成時のイベント
     def on_created(self,event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #ここはそれぞれの処理の関数
-    
+        #print("作成を検出")
     #ファイル変更時のイベント
     def on_modified(self,event):
         filepath = event.src_path
@@ -45,17 +35,17 @@ class　CheckHandler(FileSystemEventHandler):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #ここはそれぞれの処理の関数
-
+        #print("移動を検出")
     #ファイル削除時のイベント
     def on_deleted(self,event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #それぞれの処理後にあるファイルも削除
+        #print("削除を検出")
 
-
-if__name__ == "__main__":
+if __name__ == "__main__":
     #ファイルの監視を開始
-    event_handler = CheckHandler([target_file])
+    event_handler = CheckHandler()
     observer = Observer()
     observer.schedule(event_handler, target_dir, recursive=True)
     observer.start()
