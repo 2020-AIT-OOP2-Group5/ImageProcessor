1c355782cc203099cb5bb17c01b647aa57d0f0f9
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-08 13:36:47 +0900

    canny更新#1

--- ImgProcess.py
+++ ImgProcess.py
@@ -6,17 +6,17 @@ import numpy as np
 #2.顔を検出し、枠で囲う。
 
 #3.Cannyフィルタによる輪郭抽出。
+def cannyfilter(filepath):
+    # 入力画像を読み込み
+    cny_img = cv2.imread("ここはwatchdogで読み込んだファイル名")
 
-# 入力画像を読み込み
-cny_img = cv2.imread("ここはwatchdogで読み込んだファイル名")
+    # グレースケール変換
+    gray = cv2.cvtColor(cny_img, cv2.COLOR_RGB2GRAY)
 
-# グレースケール変換
-gray = cv2.cvtColor(cny_img, cv2.COLOR_RGB2GRAY)
+    #Cannyフィルタで輪郭抽出
+    cny_img = cv2.Canny(gray, 100, 200)
 
-#Cannyフィルタで輪郭抽出
-cny_img = cv2.Canny(gray, 100, 200)
-
-# 結果を出力
-cv2.imwrite("Canny_img/ここはwatchdogで読み込んだファイル名", cny_img)
+    # 結果を出力
+    cv2.imwrite("Canny_img/ここはwatchdogで読み込んだファイル名", cny_img)
 
 #4.画像のグレースケール化（出来れば2値化も）
