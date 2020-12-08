644af4e1d5684ec1729ed821c8f08244b274a0b7
Takahashi0820 <goddropdd0307@gmail.com>
2020-12-08 11:05:49 +0900

    #4 グレースケール画像の２値化
    画像の出力先変更

--- ImgProcess.py
+++ ImgProcess.py
@@ -7,11 +7,23 @@ import numpy as np
 
 #3.Cannyフィルタによる輪郭抽出。
 
+# 入力画像を読み込み
+cny_img = cv2.imread("ここはwatchdogで読み込んだファイル名")
+
+# グレースケール変換
+gray = cv2.cvtColor(cny_img, cv2.COLOR_RGB2GRAY)
+
+#Cannyフィルタで輪郭抽出
+cny_img = cv2.Canny(gray, 100, 200)
+
+# 結果を出力
+cv2.imwrite("Canny_img/ここはwatchdogで読み込んだファイル名", cny_img)
+
 #4.画像のグレースケール化（出来れば2値化も）
-img = cv2.imread("sourse.jpg",0)
+gray_img = cv2.imread("ここはwatchdogで読み込んだファイル名",0)
 
 #閾値の自動設定
-img_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+otsu_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
 
 #画像の確認
-cv2.imwrite("after_sourse.jpg",img_otsu)
+cv2.imwrite("Gray_img/ここはwatchdogで読み込んだファイル名",otsu_img)
