67524acfe652ef08e6b057ed7be389bc78bc199b
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-09 20:26:33 +0900

    Update ImgProcess.py

--- ImgProcess.py
+++ ImgProcess.py
@@ -12,18 +12,15 @@ from watchdog.events import FileSystemEventHandler
 #3.Cannyフィルタによる輪郭抽出。
 
 #4.画像のグレースケール化（出来れば2値化も）
-def grayscale(filepath):
+def grayscale(filename):
     #カレントディレクトリの移動
     os.chdir("./upload_img")
     gray_img = cv2.imread(filename,0)
 
-    #閾値の自動設定
-    otsu_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
-    
     #カレントディレクトリの移動
     os.chdir("../Gray_img")
 
     #画像の確認
-    cv2.imwrite(filename,otsu_img)
+    cv2.imwrite(filename,gray_img)
     #カレントディレクトリの移動
-    os.chdir("../")
+    os.chdir("../") 
