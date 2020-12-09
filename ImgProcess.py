b508018abb97984fe7bd3731b5e4bde723961b74
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-09 20:25:54 +0900

    Update ImgProcess.py

--- ImgProcess.py
+++ ImgProcess.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import time
+import os.path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -23,25 +24,31 @@ class ChangeHandler(FileSystemEventHandler):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #ここはそれぞれの処理の関数
-        #print("作成を検出")
+        print("作成を検出")
+        ext = os.path.splitext(filename)[1]
+        if ext == ".png" or ext == ".jpeg" or ext == ".jpg":
+            
     #ファイル変更時のイベント
     def on_modified(self,event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #ここはそれぞれの処理の関数
-    
+        print("変更を検出")
+        ext = os.path.splitext(filename)[1]
+        if ext == ".png" or ext == ".jpeg" or ext == ".jpg":
+            
     #ファイル移動時のイベント
     def on_moved(self,event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #ここはそれぞれの処理の関数
-        #print("移動を検出")
+        print("移動を検出")
     #ファイル削除時のイベント
     def on_deleted(self,event):
         filepath = event.src_path
         filename = os.path.basename(filepath)
         #それぞれの処理後にあるファイルも削除
-        #print("削除を検出")
+        print("削除を検出")
 
 if __name__ == "__main__":
     #ファイルの監視を開始
