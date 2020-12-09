30bf304debc1d3efb3557d8ce9829cae56ab97d5
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-09 20:33:21 +0900

    Update ImgProcess.py

--- ImgProcess.py
+++ ImgProcess.py
@@ -6,15 +6,44 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os.path
 
-target_dir = "./upload_img"
-
 #1.顔を検出し、モザイク処理。
 
 #2.顔を検出し、枠で囲う。
 
 #3.Cannyフィルタによる輪郭抽出。
+def cannyfilter(filename):
+    #カレントディレクトリの移動
+    os.chdir("./upload_img")
+
+    # 入力画像を読み込み
+    cny_img = cv2.imread(filename)
+
+    # グレースケール変換
+    gray = cv2.cvtColor(cny_img, cv2.COLOR_RGB2GRAY)
+
+    #Cannyフィルタで輪郭抽出
+    cny_img = cv2.Canny(gray, 100, 200)
+
+    #カレントディレクトリの移動
+    os.chdir("../Canny_img")
+    # 結果を出力
+    cv2.imwrite(filename, cny_img)
+    #カレントディレクトリの移動
+    os.chdir("../")
 
 #4.画像のグレースケール化（出来れば2値化も）
+def grayscale(filename):
+    #カレントディレクトリの移動
+    os.chdir("./upload_img")
+    gray_img = cv2.imread(filename,0)
+
+    #カレントディレクトリの移動
+    os.chdir("../Gray_img")
+
+    #画像の確認
+    cv2.imwrite(filename,gray_img)
+    #カレントディレクトリの移動
+    os.chdir("../") 
 
 #watchdogを実装
 
